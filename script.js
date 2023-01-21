3dc4e3cbddf36d43ec0dc957c5d9e7928e84ee11
pablokayro <pablokayro2413@gmail.com>
2023-01-21 19:09:54 -0300

    initial commit

---
+++ script.js
@@ -0,0 +1,23 @@
+const form = document.querySelector("#form-habits")
+const nlwSetup = new NLWSetup(form)
+
+const button = document.querySelector("header button")
+button.addEventListener("click", add)
+form.addEventListener("change", save)
+
+function add() {
+  const today = new Date().toLocaleDateString("pt-br").slice(0, -5)
+  const dayExist = nlwSetup.dayExists(today)
+  if (dayExist) {
+    alert("Dia ja incluso!🔴")
+    return
+  }
+  alert("Adicionado com sucesso!🟢")
+  nlwSetup.addDay(today)
+}
+function save() {
+  localStorage.setItem("NLWSetup@habits", JSON.stringify(nlwSetup.data))
+}
+const data = JSON.parse(localStorage.getItem("NLWSetup@habits")) || {}
+nlwSetup.setData(data)
+nlwSetup.load()
